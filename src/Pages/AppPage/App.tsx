3bbcf922d59f3dce1b6537264a7f1e3544c1bf70
omerbenda <34948058+omerbenda@users.noreply.github.com>
2024-07-27 15:38:22 +0300

    feat(App.tsx, tailwind.config.js): Add tailwind and a basic structure for the app

--- src/Pages/AppPage/App.tsx
+++ src/Pages/AppPage/App.tsx
@@ -1,5 +1,17 @@
 const AppPage = () => {
-  return <div>Game of Life</div>;
+  return (
+    <div className="flex flex-col h-full bg-gray-300">
+      <div className="flex items-center bg-gray-600 h-32">
+        <div className="w-8" />
+        <div className="text-white text-4xl">Game of Life</div>
+      </div>
+      <div className="flex flex-col flex-grow">
+        <div className="flex justify-center items-center flex-grow w-full">
+          <div className="border-2 border-black bg-white w-3/4 h-3/4"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AppPage;
